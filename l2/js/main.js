87c7f64279c39ad2fe4862fabfe55b1904e506c6
serpstrong <ommuts@gmail.com>
2020-12-21 15:09:56 +0500

    l2

--- l2/js/main.js
+++ l2/js/main.js
@@ -1,11 +1,12 @@
-class ProductsList{
-    constructor(container = '.products'){
+class ProductsList {
+    constructor(container = '.products') {
         this.container = container;
         this.goods = [];//массив товаров
 //        this.allProducts=[];//массив объектов
         this._fetchProducts();
     }
-    _fetchProducts(){
+
+    _fetchProducts() {
         this.goods = [
             {id: 1, title: 'Notebook', price: 2000},
             {id: 2, title: 'Mouse', price: 20},
@@ -13,26 +14,36 @@ class ProductsList{
             {id: 4, title: 'Gamepad', price: 50},
         ];
     }
-    
-    render(){
+
+    render() {
         const block = document.querySelector(this.container);
-        for(let product of this.goods){
+        for (let product of this.goods) {
             const productObject = new ProductItem(product);
-//            this.allProducts.push(productObject);
-//            block.innerHTML += productObject.render();
-              block.insertAdjacentHTML('beforeend',productObject.render());
+            block.insertAdjacentHTML('beforeend', productObject.render());
         }
     }
+
+    priceList() {
+        let sum = 0;
+        this.goods.forEach((arrayItem) => {
+            for (let key in arrayItem) {
+            }
+            sum += arrayItem.price;
+        });
+        return console.log(sum);
+    }
+
 }
 
-class ProductItem{
-    constructor(product,img='https://placehold.it/200x150'){
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
         this.title = product.title;
         this.price = product.price;
         this.id = product.id;
         this.img = img;
     }
-    render(){
+
+    render() {
         return `<div class="product-item">
                 <img src="${this.img}">
                 <h3>${this.title}</h3>
@@ -42,6 +53,17 @@ class ProductItem{
     }
 }
 
+class Basket {
+
+}
+
+class good {
+
+}
+
 let list = new ProductsList();
 list.render();
+
+let Price = new ProductsList();
+Price.priceList();
 
